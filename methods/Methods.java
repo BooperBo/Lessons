579b77c0c0ee5028823842459686573a0f88ace5
BusyginDV <dennzelw@gmail.com>
2021-08-01 02:24:44 +0500

    lesson

--- methods/Methods.java
+++ methods/Methods.java
@@ -5,12 +5,30 @@ public class Methods {
     public static void main(String[] args) {
 
         printMessage("Denis");
+        System.out.println("Rectangle square = " + calcRectangleSquare(5, 10));
+        System.out.println("Square = " + calcSquare(10));
+        System.out.println("Sum of squares= " + (calcSquare(10) + calcRectangleSquare(5, 10)));
 
-        int a = 5;
-        int b = 10;
-        System.out.println("Rectangle square = " + calcRectangleSquare(a,b));
-        System.out.println("Square = " + calcSquare(b));
-        System.out.println("Sum of squares= " + (calcSquare(b)+calcRectangleSquare(a,b)));
+
+        String str = "I like black coffee";
+        String str3 = "I like coffee!!!";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+        String str2 = "I like coffee";
+        String str4 = new String("I like coffee");
+        String substring = str3.substring(0,13);
+        System.out.println("Substring = " + substring);
+
+        boolean b = str2.equals(substring);
+        System.out.println(b);
+
+        boolean like = str.endsWith("fee!");
+        System.out.println(like);
+
+        System.out.println(str.replace("black", "white"));
+
+        System.out.println(str3.indexOf("abc"));
     }
 
     static void printMessage(String name) {
