f75d526be12c215937dc060ed3f5e3fe65b99a95
BusyginDV <dennzelw@gmail.com>
2021-08-01 00:25:14 +0500

    Первые уроки продолжение

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -11,7 +11,10 @@ public class HelloWorld {
         float myFloatValue = 23.56f;
         double myDoubleValue = 23.54d;
         double sum = a + b + c + myDoubleValue + myFloatValue + myLongValue;
+        float floatResult = myFloatValue / 2;
+        System.out.println("floatResult" + floatResult);
 
+        System.out.println("sum = " + sum);
         System.out.println("b = " + b);
         System.out.println("a = " + a);
         System.out.println("c = " + c);
