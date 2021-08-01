65378419ea802b7fb232eb119ecef3e9446c8329
BusyginDV <dennzelw@gmail.com>
2021-08-01 14:40:55 +0500

    lesson

---
+++ constructors/ConstructorsMain.java
@@ -0,0 +1,13 @@
+package constructors;
+
+import domainmodel.Employee;
+
+public class ConstructorsMain {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Denis", "Developer", 100);
+        System.out.println(employee);
+
+    }
+}
