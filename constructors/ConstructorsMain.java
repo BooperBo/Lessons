54a15e3b9a04d5baaa17603b7536c9bf836e96f2
BusyginDV <dennzelw@gmail.com>
2021-08-01 14:57:34 +0500

    lesson

--- constructors/ConstructorsMain.java
+++ constructors/ConstructorsMain.java
@@ -6,8 +6,13 @@ public class ConstructorsMain {
 
     public static void main(String[] args) {
 
-        Employee employee = new Employee("Denis", "Developer", 100);
+        Employee employee = new Employee();
+       /* Employee employee2 = new Employee("Denis", "Developer", 100);
+        Employee employee3 = new Employee("Kostya", "Developer", 100);
         System.out.println(employee);
+        System.out.println(employee2);
+        System.out.println(employee3);*/
+
 
     }
 }
--- domainmodel/Employee.java
+++ domainmodel/Employee.java
@@ -3,21 +3,40 @@ package domainmodel;
 public class Employee {
 
     private static int id;
+    private int employeeId;
     private String name;
     private String position;
     private int salary;
+    private String department;
 
+    static {
+        id = 1001;
+        System.out.println("static init block called");
+    }
 
-    public Employee(String name, String position) {
-        this.name = name;
-        this.position = position;
+    {
+        department = "IT";
+        System.out.println("Non-static init block called");
+    }
+
+    public Employee() {
+        this("A", "B", 1);
+        System.out.println("Empty constructor called");
     }
 
     public Employee(String name, String position, int salary) {
-        id = id + 1;
+        this(name, position, salary, "IT");
+        System.out.println("Constructor with 3 params called");
+
+    }
+
+    public Employee(String name, String position, int salary, String department) {
+        employeeId = id++;
         this.name = name;
         this.position = position;
         this.salary = salary;
+        this.department = department;
+        System.out.println("Constructor with 4 params called");
     }
 
     public static int getId() {
@@ -39,7 +58,8 @@ public class Employee {
     @Override
     public String toString() {
         return "Employee{" +
-                "name='" + name + '\'' +
+                "employeeId=" + employeeId +
+                ", name='" + name + '\'' +
                 ", position='" + position + '\'' +
                 ", salary=" + salary +
                 '}';
