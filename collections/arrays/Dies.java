c785d961e446dc7222914a0bcf03a110c1cdd8d7
BusyginDV <dennzelw@gmail.com>
2021-08-06 20:04:41 +0500

    lesson

--- collections/arrays/Dies.java
+++ collections/arrays/Dies.java
@@ -10,7 +10,6 @@ public class Dies {
         int[] frequency = new int[7];
         for (int i = 0; i < 1000; i++) {
             ++frequency[1 + random.nextInt(6)];
-
         }
         for (int i = 1; i < frequency.length; i++) {
             System.out.println("Side " + i + " : " + frequency[i]);
