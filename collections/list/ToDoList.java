7adf59732a3936ca67cf181f52b372fc094da6be
BusyginDV <dennzelw@gmail.com>
2021-08-13 14:16:47 +0500

    lesson

--- collections/list/ToDoList.java
+++ collections/list/ToDoList.java
@@ -1,14 +1,33 @@
 package collections.list;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ToDoList {
 
-    private List<String> toDoList = new ArrayList<>();
+    private LinkedList<String> toDoList = new LinkedList<>();
 
     public void addToList(String task) {
+//        toDoList.add(task);
+        addInAlphabeticalOrder(task);
+    }
+
+    private boolean addInAlphabeticalOrder(String task) {
+        ListIterator<String> listIter = toDoList.listIterator();
+        while (listIter.hasNext()) {
+            int compared = listIter.next().compareTo(task);
+            if (compared == 0) {
+                System.out.println("Task already exists in the list");
+                return true;
+            } else if (compared > 0) {
+                listIter.previous();
+                listIter.add(task);
+                return true;
+            }
+        }
         toDoList.add(task);
+        return true;
     }
 
     public void addToListAtPosition(int position, String task) {
@@ -16,8 +35,13 @@ public class ToDoList {
     }
 
     public void printToDoList() {
-        for (int i = 0; i < toDoList.size(); i++) {
+        /*for (int i = 0; i < toDoList.size(); i++) {
             System.out.println(i + " - " + toDoList.get(i));
+        }*/
+
+        Iterator<String> iterator = toDoList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Element " + iterator.next());
         }
     }
 
