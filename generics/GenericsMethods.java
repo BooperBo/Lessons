3c2e8a8c3cd62ae6f490314096faed25fadd7f3a
BusyginDV <dennzelw@gmail.com>
2021-09-04 19:33:35 +0500

    lesson

--- generics/GenericsMethods.java
+++ generics/GenericsMethods.java
@@ -2,6 +2,7 @@ package generics;
 
 public class GenericsMethods {
     public static void main(String[] args) {
+
         Integer[] intArray = {1, 2, 0, 8, 5, 6};
         Double[] doubleArray = {0.234, 6.45, 9.034, 1.345, 4.675};
         String[] stringArray = {"abc", "vfr", "rtw", "ert"};
@@ -10,31 +11,29 @@ public class GenericsMethods {
         printArray(intArray);
         printArray(doubleArray);
         printArray(stringArray);
+
+        System.out.println("Max for array of integers "+findMax(intArray));
+        System.out.println("Max for array of integers "+findMax(doubleArray));
+        System.out.println("Max for array of integers "+findMax(stringArray));
     }
 
-    private static void printArray(Integer[] intArray) {
+    private static <T> void printArray(T[] intArray) {
         System.out.println("Non generics methods called");
-        for (Integer element : intArray) {
-            System.out.printf("%s, ", element);
-        }
-        System.out.println();
-    }
-    private static void printArray(Double[] intArray) {
-        System.out.println("Non generics methods called");
-        for (Double element : intArray) {
-            System.out.printf("%s, ", element);
-        }
-        System.out.println();
-    }
-    private static void printArray(String[] intArray) {
-        System.out.println("Non generics methods called");
-        for (String element : intArray) {
+        for (T element : intArray) {
             System.out.printf("%s, ", element);
         }
         System.out.println();
     }
 
+    private static <T extends Comparable<T>> T findMax(T[] array) {
+        T max = array[0];
+        for (T e : array) {
+            if (e.compareTo(max)>0) {
+                max = e;
 
-
+            }
+        }
+        return max;
+    }
 
 }
